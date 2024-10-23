188709d6b1a3b73cf36c99b57ed5b2910811e6e7
zhaolong <1476950334@qq.com>
2024-10-23 14:17:41 +0800

    Changes

---
+++ src/test/java基础/A04_SimpleDateFormat.java
@@ -0,0 +1,21 @@
+package test.java基础;
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class A04_SimpleDateFormat {
+
+    public static void main(String[] args) {
+        String str="2000-11-11";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date date = simpleDateFormat.parse(str);
+            String yyyy年MM月dd日 = new SimpleDateFormat("yyyy年MM月dd日").format(date);
+            System.out.println(yyyy年MM月dd日);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
